be03dbe2951deb5ddc787f5197656a39e3f6236e
Graut <9389465+GrautDevelopes@users.noreply.github.com>
2017-07-22 21:58:57 -0400

    Blobar url extraction is longer domain relative.

--- Apul.py
+++ Apul.py
@@ -19,9 +19,10 @@ def clean_text(rgx, text):
     new_text = re.sub(rgx, '', new_text)
     return new_text
 if "http://blobar.org/d/p" in RedirectorResponseHTML:
- print("[Apul] Verifed " + sys.argv[1] + " !")
- while True:
-        trim1 = RedirectorResponseHTML[614:]
+    print("[Apul] Verifed " + sys.argv[1] + " !")
+    while True:
+        trim0 = clean_text('.*var u = \'', RedirectorResponseHTML)
+        trim1 = clean_text('\'\+\(\(r.*', trim0)
         req2 = urllib2.Request(clean_text('\'\+\(\(.*', trim1))
         Redirector2Response = urllib2.urlopen(req2)
         Redirecter3url = Redirector2Response.geturl() + "&z=240"
@@ -31,8 +32,12 @@ if "http://blobar.org/d/p" in RedirectorResponseHTML:
         Redirector3Response = urllib2.urlopen(req3)
         PopUpHTML = Redirector3Response.read() #TODO: Extract phone number from popup
         PopUpURL = Redirector3Response.geturl()
-        num = re.match("(\+|1-|8..).*", PopUpHTML)
-        print(PopUpURL)
+        num = re.search(r"^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})", PopUpHTML)#.group(0) #Much thanks Laurence O'Donnell from regexlib
         Redirector3Response.close()
- else:
-	      print("[Apul] The redirector " + sys.argv[1] + " does not use blobar.")
+        if num:
+            #print(PopUpURL + " | " + num)
+			print(PopUpURL + " | ".format(num.group(0)))
+        else:
+            print(PopUpURL)
+else:
+    print("[Apul] The redirector " + sys.argv[1] + " does not use blobar.")
